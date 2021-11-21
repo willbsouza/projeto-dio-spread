d1b96db0255e875950298052604fb895e70a792d
Willams Souza <willamsbernardo@hotmail.com>
2021-11-21 20:36:03 -0300

    Add class Mentoria, Curso.

---
+++ src/br/com/dio/projetospread/dominio/Curso.java
@@ -0,0 +1,50 @@
+package br.com.dio.projetospread.dominio;
+
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    private Integer cargaHoraria;
+
+    public Curso() {
+    }
+
+    public Curso(String titulo, String descricao, Integer cargaHoraria) {
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Integer getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(Integer cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
